e799141a2a62d00e8ab1c5e0e13bd716867d96f2
suvambasak <suvambasak2008@gmail.com>
2021-05-11 15:45:34 +0530

    API

---
+++ Demo/TS_sub.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+import json
+
+URL = 'https://api.thingspeak.com/channels/1385093/feeds.json?results=1'
+
+
+with urlopen(URL) as url:
+    data = json.loads(url.read().decode())
+    print(data['feeds'][-1])
+    print('Temp: ', data['feeds'][-1]['field1'])
+    print('Hume: ', data['feeds'][-1]['field2'])
+    print('Date: ', data['feeds'][-1]['created_at'].split('T')[0])
+    print('Time: ', data['feeds'][-1]['created_at'].split('T')[1])
+    # print(type(data))
